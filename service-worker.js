030ef43e21ba2996ba5044b4b00f3bf816cbeb38
anastasiia <anastasiia.aliinyk@gmail.com>
2021-03-15 17:56:04 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2864373d6bc61c7e48f9e43b143db318.js"
+  "./precache-manifest.0bdb2ffdf918c88ad4bc2b52e0cd19e6.js"
 );
 
 self.addEventListener('message', (event) => {
